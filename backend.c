cf9795d737efcc6b018e0953d41a6f86f8b539de
rgrunsten <rgrunsten64@gmail.com>
2021-04-30 18:40:03 -0400

    v 0.1

--- backend.c
+++ backend.c
@@ -3,31 +3,31 @@
 #include <string.h>
 #include <curl/curl.h>
 
-char urlBaseCases[50] = "https://covid-api.mmediagroup.fr/v1/cases?country=";
-char urlBaseVaccines[53] = "https://covid-api.mmediagroup.fr/v1/vaccines?country=";
+char urlBase[45] = "https://covid-api.mmediagroup.fr/v1/";
 
-void fetchQuery(const char* url, const char* countryCacheName) {
+void fetchQuery(const char* url, const char* outputFileName) {
   CURL* easyhandle = curl_easy_init();
   curl_easy_setopt( easyhandle, CURLOPT_URL, url );
-  FILE* file = fopen( countryCacheName, "w" );
+  FILE* file = fopen( outputFileName, "w" );
   curl_easy_setopt( easyhandle, CURLOPT_WRITEDATA, file);
   curl_easy_perform( easyhandle );
   curl_easy_cleanup( easyhandle );
 }
 
-void setQuery(const char* country) {
+void setQuery(const char* country, const char* category) {
   FILE *stream;
   char *buf;
   size_t len;
   stream = open_memstream(&buf, &len);
-  fprintf(stream, "%s%s", urlBaseCases, country);
-  //strcat(urlBaseCases, country);
-  //strcat(urlBaseVaccines, country);
+  fprintf(stream, "%s%s?country=%s", urlBase, category, country);
   fclose(stream);
   printf("%s", buf);
-  fetchQuery(buf, "hardcode");
+  char fileName[50];
+  strcpy(fileName, country);
+  strcat(fileName, category);
+  fetchQuery(buf, fileName);
 }
 
 int main(void) {
-  setQuery("France");
+  setQuery("France", "cases");
 }
